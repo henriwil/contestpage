cd89faac4100d4dcaa7b13c48a4e65ec114fc652
Henrik Appear.in <henriklokenwille@gmail.com>
2025-10-03 13:43:06 +0200

    Update debug.js

--- netlify/functions/debug.js
+++ netlify/functions/debug.js
@@ -2,8 +2,8 @@ export async function handler() {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      SITE_ID: process.env.NETLIFY_SITE_ID ? "exists" : "missing",
-      BLOBS_TOKEN: process.env.NETLIFY_BLOBS_TOKEN ? "exists" : "missing",
+      SITE_ID: !!process.env.NETLIFY_SITE_ID,
+      BLOBS_TOKEN: !!process.env.NETLIFY_BLOBS_TOKEN,
     }),
   };
 }
