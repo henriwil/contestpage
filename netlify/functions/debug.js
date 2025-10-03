ee2633a4b17d9ef14c06417b11e66690d1e7f8d3
Henrik Appear.in <henriklokenwille@gmail.com>
2025-10-03 13:33:01 +0200

    Create debug.js

---
+++ netlify/functions/debug.js
@@ -0,0 +1,9 @@
+export async function handler() {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      SITE_ID: process.env.NETLIFY_SITE_ID ? "exists" : "missing",
+      BLOBS_TOKEN: process.env.NETLIFY_BLOBS_TOKEN ? "exists" : "missing",
+    }),
+  };
+}
